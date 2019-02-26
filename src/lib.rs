41458669827cccc45f337d6fa50b3c4ef72e328a
William Ben Glaeser <ben_glaeser@hotmail.de>
2019-02-26 11:26:22 +0100

    exclude symlinks and refactor code

--- src/lib.rs
+++ src/lib.rs
@@ -27,59 +27,35 @@ impl Directory {
         for p in fs::read_dir(&self.dir_name).unwrap() {
 
             let path = p.unwrap();
-            let path_string = &path.file_name();
             let metadata = fs::metadata(&path.path()).unwrap();
 
-            if metadata.is_dir() {
+            // Add raw bytes
+            let path_string = String::from(path.file_name().to_str().unwrap());
+            let path_name =
+                format!("{}/{}", &self.dir_name, path_string);
+            let this_v = fs::metadata(path_name.clone()).unwrap().len();
+            self.size += this_v.clone();
 
-                // Parse Sub Directory
-                let new_dir_name =
-                    format!("{}/{}", &self.dir_name, String::from(path_string.to_str().unwrap()));
-                let mut new_dir = Directory::initialise(new_dir_name.clone());
-                let this_v = fs::metadata(new_dir_name.clone()).unwrap().len();
-                self.size += this_v.clone();
+            if metadata.is_dir()  {
 
+                match fs::read_link(path_name.clone()) {
+                    Ok(_f) => {}
+                    Err(_e) => {
+                        // Parse Sub Directory
+                        let mut new_dir = Directory::initialise(path_name.clone());
+                        new_dir.parse_dir();
 
-                new_dir.parse_dir();
-
-                // Update Dir Values
-                self.size += new_dir.size;
-                self.sub_directories.push(new_dir);
+                        // Update Dir Values
+                        self.size += new_dir.size;
+                        self.sub_directories.push(new_dir);
+                    }
+                }
             }
-
             else if metadata.is_file() {
-
-                // Update Files
-                let filename_string =
-                    String::from(path_string.to_str().unwrap()).clone();
-                self.files.push(filename_string.clone());
-                let filename_full =
-                    format!("{}/{}", &self.dir_name, filename_string);
-                self.size += fs::metadata(filename_full).unwrap().len();
+                self.files.push(path_name.clone());
             }
         }
         print!("{}: {}\n", self.dir_name, self.size);
     }
 }
 
-impl Directory {
-    pub fn sum_filesizes(&self) {
-        let mut total = 0;
-
-        for v in &self.files {
-            let filename_full = format!("{}/{}", &self.dir_name, v);
-            let metadata = fs::metadata(filename_full).unwrap();
-            total += metadata.len();
-        }
-
-        for d in &self.sub_directories {
-            total += d.size;
-        }
-        print!("{}: {}\n", &self.dir_name, total);
-    }
-}
-
-
-//impl Clone for Directory {
-//    fn clone(&self) -> Directory { *self }
-//}
